a9877218aa5538e6471b415e000980abad01e8b5
Jakub Idziak <j.idziak@macopedia.pl>
2022-06-23 13:27:09 +0200

    Simplified currencies controller

--- src/Controller/Shop/CurrenciesController.php
+++ src/Controller/Shop/CurrenciesController.php
@@ -8,22 +8,15 @@ use App\Services\CurrencyService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\AbstractType;
 
 #[Route('/shop')]
 class CurrenciesController extends AbstractController
 {
-    public function __construct(
-        private CurrencyRepository $currencyRepository
-    ) {
-    }
-
     #[Route('/currencies-list', name: 'currencies_list', methods: ['GET'])]
-    public function list(Request $request): Response
+    public function list(CurrencyRepository $currencyRepository): Response
     {
-        $currencies = $this->currencyRepository->findByShopId($this->getUser()->getShopId());
+        $currencies = $currencyRepository->findByShopId($this->getUser()->getShopId());
 
         return $this->renderForm('currencies/list.html.twig', [
             'currencies' => $currencies
