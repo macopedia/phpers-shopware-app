a9d138badad1891a60d178afe686f59f41f1e577
Jakub Idziak <j.idziak@macopedia.pl>
2022-06-23 10:59:57 +0200

    Created currency migration, entity and repository

---
+++ src/Entity/Currency.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CurrencyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CurrencyRepository::class)]
+class Currency
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\ManyToOne(targetEntity: Shop::class, inversedBy: 'currencies')]
+    #[ORM\JoinColumn(name: 'shop_id', referencedColumnName: 'shop_id')]
+    private Shop $shop;
+
+    #[ORM\Column(type: 'string')]
+    private string $code;
+
+    #[ORM\Column(type: 'float')]
+    private float $rate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getShop(): Shop
+    {
+        return $this->shop;
+    }
+
+    public function setShop(Shop $shop): self
+    {
+        $this->shop = $shop;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getRate(): ?float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(float $rate): self
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+}
